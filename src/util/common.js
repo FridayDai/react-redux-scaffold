bce4d00c30573f39b83018cb052dea960c739b95
fridaydai <875606747@qq.com>
2018-04-03 17:25:07 +0800

    update util

--- src/util/common.js
+++ src/util/common.js
@@ -31,3 +31,33 @@ export const decryptPwd = (pwd) => {
     var decrypt = CryptoJS.AES.decrypt(pwd, key, {mode:CryptoJS.mode.ECB,padding: CryptoJS.pad.Pkcs7});
     return CryptoJS.enc.Utf8.stringify(decrypt).toString();
 };
+
+export const whitchType = (data) => {
+    const type = Object.prototype.toString.call(data);
+    switch (type) {
+        case "[object Null]":
+            return 'Null';
+        case "[object Undefined]":
+            return 'Undefined';
+        case "[object Number]":
+            return 'Number';
+        case "[object Boolean]":
+            return 'Boolean';
+        case "[object String]":
+            return 'String';
+        case "[object Object]":
+            return 'Object';
+        case "[object Symbol]":
+            return 'Symbol';
+        case "[object Array]":
+            return 'Array';
+        case "[object Function]":
+            return 'Function';
+        case "[object Date]":
+            return 'Date';
+        case "[object Math]":
+            return 'Math';
+        default:
+            return '';
+    }
+};
