a217342e221ae9a31e9cee4a9310f3807a45e0e3
yi.dai <yi.dai@envisioncn.com>
2019-01-22 11:47:50 +0800

    (Issue#)update eslint

--- src/components/Common/Loading.js
+++ src/components/Common/Loading.js
@@ -1,13 +1,23 @@
 /**
  * Created by yi.dai on 2018/12/21.
  */
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 
-export default class Loading extends PureComponent {
+export default class Loading extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //
+    };
+  }
+
   render() {
     return (
-      <div style={{ background: 'red' }}>
-        {this.props.Loading}
+      <div style={{
+        fontSize: '16px', background: 'red', width: '200px', height: '200px',
+      }}
+      >
+        {'hello'}
       </div>
     );
   }
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -5,125 +5,128 @@ import React, { Component } from 'react';
 // import {fetchTopics, testAction} from '../actions/index';
 import { connect } from 'react-redux';
 import Miment from 'miment';
-import Pages from '../components/HomePage/Pages';
 import './HomePage.css';
 import { browserHistory } from 'react-router';
 import '../common/style.css';
 import hljs from 'highlightjs';
+import Pages from '../components/HomePage/Pages';
 import '../common/highlight-default.css';
-import {getDocList} from '../actions/index';
+import { getDocList } from '../actions/index';
 
 class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    componentDidMount() {
-        const {dispatch} = this.props;
-        dispatch(getDocList());
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(getDocList());
 
-        document.querySelectorAll('pre code').forEach((item) => {
-            hljs.highlightBlock(item);
-        });
+    document.querySelectorAll('pre code').forEach((item) => {
+      hljs.highlightBlock(item);
+    });
 
-        hljs.initHighlightingOnLoad();
-        console.log(Miment().format());
-    }
+    hljs.initHighlightingOnLoad();
+    console.log(Miment().format());
+  }
 
-    render() {
-        const {dispatch, fetchTopicsReducer, docReducer} = this.props;
-        const docList = docReducer.docList || {};
-        const test = `const Koa = require('koa');
+  render() {
+    const { dispatch, docReducer } = this.props;
+    const docList = docReducer.docList || {};
+    const test = `const Koa = require('koa');
 const app = new Koa(); 
 app.use(async ctx => { 
     ctx.body = 'Hello World'; 
 });  
 app.listen(3000);`;
 
-        return (
-            <div className='homepage-content'>
-                <Pages
-                    props={this.props}
-                    dispatch={dispatch}
-                    docList={docList}
-                />
-                <section className='section-part'>
-                    <h1 id='introduction'>Introduction</h1>
-                    <div className='color-grey'>
-                        Koa is a new web framework designed by the team behind Express, which aims to be a smaller, more expressive, and more robust foundation for web applications and APIs. Through leveraging generators Koa allows you to ditch callbacks and greatly increase error-handling. Koa does not bundle any middleware within core, and provides an elegant suite of methods that make writing servers fast and enjoyable.
-                    </div>
-                </section>
-                {/*<section className='section-part'>*/}
-                    {/*<h1 id='introduction'>轮播图</h1>*/}
-                    {/*<ImgSlider*/}
-                        {/*imgs={imgs}*/}
-                        {/*// speed={2}*/}
-                        {/*// autoplay={true}*/}
-                        {/*dots={true}*/}
-                        {/*arrows={true}*/}
-                    {/*/>*/}
-                {/*</section>*/}
-                <section className='section-part'>
-                    <h1 id='installation'>Installation</h1>
-                    <div className='color-grey'>
+    return (
+      <div className="homepage-content">
+        <Pages
+          props={this.props}
+          dispatch={dispatch}
+          docList={docList}
+        />
+        <section className="section-part">
+          <h1 id="introduction">Introduction</h1>
+          <div className="color-grey">
+            Koa is a new web framework designed by the team behind Express, which aims to be a smaller, more expressive, and more robust foundation for web applications and APIs. Through leveraging generators Koa allows you to ditch callbacks and greatly increase error-handling. Koa does not bundle any middleware within core, and provides an elegant suite of methods that make writing servers fast and enjoyable.
+          </div>
+        </section>
+        {/* <section className='section-part'> */}
+        {/* <h1 id='introduction'>轮播图</h1> */}
+        {/* <ImgSlider */}
+        {/* imgs={imgs} */}
+        {/* // speed={2} */}
+        {/* // autoplay={true} */}
+        {/* dots={true} */}
+        {/* arrows={true} */}
+        {/* /> */}
+        {/* </section> */}
+        <section className="section-part">
+          <h1 id="installation">Installation</h1>
+          <div className="color-grey">
                         Koa requires node v7.6.0 or higher for ES2015 and async function support.
-                    </div>
-                    <div className='color-grey'>
+          </div>
+          <div className="color-grey">
                         You can quickly install a supported version of node with your favorite version manager:
-                    </div>
-                    <pre>
-                        <code className="hljs">
-                            $ nvm install 7 <br></br>
-                            $ npm i koa <br></br>
+          </div>
+          <pre>
+            <code className="hljs">
+                            $ nvm install 7
+              {' '}
+              <br />
+                            $ npm i koa
+              {' '}
+              <br />
                             $ node my-koa-app.js
-                        </code>
-                    </pre>
-                </section>
-                <section className='section-part'>
-                    <h1 id='application'>Application</h1>
-                    <div className='color-grey'>
-                        A Koa application is an object containing an array of middleware functions which are composed and executed in a stack-like manner upon request. Koa is similar to many other middleware systems that you may have encountered such as Ruby's Rack, Connect, and so on - however a key design decision was made to provide high level "sugar" at the otherwise low-level middleware layer. This improves interoperability, robustness, and makes writing middleware much more enjoyable.
+            </code>
+          </pre>
+        </section>
+        <section className="section-part">
+          <h1 id="application">Application</h1>
+          <div className="color-grey">
+              A Koa application is an object containing an array of middleware functions which are composed and executed in a stack-like manner upon request. Koa is similar to many other middleware systems that you may have encountered such as Ruby's Rack, Connect, and so on - however a key design decision was made to provide high level "sugar" at the otherwise low-level middleware layer. This improves interoperability, robustness, and makes writing middleware much more enjoyable.
 
-                        This includes methods for common tasks like content-negotiation, cache freshness, proxy support, and redirection among others. Despite supplying a reasonably large number of helpful methods Koa maintains a small footprint, as no middleware are bundled.
+              This includes methods for common tasks like content-negotiation, cache freshness, proxy support, and redirection among others. Despite supplying a reasonably large number of helpful methods Koa maintains a small footprint, as no middleware are bundled.
 
-                        The obligatory hello world application:
-                    </div>
-                    <pre>
-                        <code className="hljs">
-                            {test}
-                        </code>
-                    </pre>
-                </section>
-                {
-                    Object.keys(docList).reverse().map((doc) => (
-                        <section className='section-part'>
-                            <h1
-                                style={{'cursor': 'pointer'}}
-                                className="hover-underline-animation"
-                                id={doc}
-                                onClick={() => {
-                                    browserHistory.push(`/${doc}`);
-                                }}
-                            >
-                                {docList[doc].name || ''}
-                            </h1>
-                            <h5>
-                                <span className='label-html'>HTML</span>
-                                <span className='label-css'>CSS</span>
-                                <span className='label-js'>JAVASCRIPT</span>
-                                <span className='label-linux'>Linux</span>
-                            </h5>
-                            <div className='color-grey'>{docList[doc].desc || ''}</div>
-                        </section>
-                    ))
-                }
-            </div>
-        );
-    }
+              The obligatory hello world application:
+          </div>
+          <pre>
+            <code className="hljs">
+              {test}
+            </code>
+          </pre>
+        </section>
+        {
+          Object.keys(docList).reverse().map(doc => (
+            <section className="section-part">
+              <h1
+                style={{ cursor: 'pointer' }}
+                className="hover-underline-animation"
+                id={doc}
+                onClick={() => {
+                  browserHistory.push(`/${doc}`);
+                }}
+              >
+                {docList[doc].name || ''}
+              </h1>
+              <h5>
+                <span className="label-html">HTML</span>
+                <span className="label-css">CSS</span>
+                <span className="label-js">JAVASCRIPT</span>
+                <span className="label-linux">Linux</span>
+              </h5>
+              <div className="color-grey">{docList[doc].desc || ''}</div>
+            </section>
+          ))
+        }
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return state;
-};
+const mapStateToProps = state => state;
 
-export default connect(mapStateToProps)(HomePage)
+export default connect(mapStateToProps)(HomePage);
