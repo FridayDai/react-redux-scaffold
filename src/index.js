dfbd16c8eee5e399d4252c537ca586004dcbe61c
yi.dai <yi.dai@envisioncn.com>
2019-01-23 15:45:58 +0800

    (Issue#)update loadable

--- src/index.js
+++ src/index.js
@@ -4,59 +4,83 @@ import { Provider } from 'react-redux';
 import {
   Router, Route, IndexRoute, browserHistory
 } from 'react-router';
-import Loadable from 'react-loadable';
+// import Loadable from 'react-loadable';
 import store from './configStore';
 // import App from './containers/App';
 // import Login from './containers/Login';
 import Comment from './containers/Comment';
 import WriteDoc from './containers/WriteDoc';
-import Loading from './components/Common/Loading';
+// import Loading from './components/Common/Loading';
 // import Sort from './util/Sort';
 // import BSTree from './util/Tree';
 
 const rootElement = document.getElementById('container');
 
-const App = Loadable({
-  'loader': () => import('./containers/App.js'),
-  'loading': Loading
-});
+// const App = Loadable({
+//   'loader': () => import('./containers/App.js'),
+//   'loading': Loading
+// });
 
-const Login = Loadable({
-  'loader': () => import('./containers/Login.js'),
-  'loading': Loading
-});
+const App = (location, cb) => {
+    require.ensure([], (require) => {
+        cb(null, require('./containers/App.js').default);
+    }, 'App');
+};
 
-const HomePage = Loadable({
-  'loader': () => import('./containers/HomePage.js'),
-  'loading': Loading
-});
+const Login = (location, cb) => {
+  require.ensure([], (require) => {
+    cb(null, require('./containers/Login.js').default);
+  }, 'Login');
+};
 
-const Doc = Loadable({
-  'loader': () => import('./containers/Doc.js'),
-  'loading': Loading
-});
+// const Login = Loadable({
+//   'loader': () => import('./containers/Login.js'),
+//   'loading': Loading
+// });
 
-const Profile = Loadable({
-  'loader': () => import('./containers/Profile.js'),
-  'loading': Loading
-});
+const HomePage = (location, cb) => {
+  require.ensure([], (require) => {
+    cb(null, require('./containers/HomePage.js').default);
+  }, 'HomePage');
+};
 
-// const Profile = (location, cb) => {
-//     require.ensure([], require => {
-//         cb(null, require('./containers/Profile.js').default)
-//     },'Profile')
-// };
+// const HomePage = Loadable({
+//   'loader': () => import('./containers/HomePage.js'),
+//   'loading': Loading
+// });
+
+const Doc = (location, cb) => {
+  require.ensure([], (require) => {
+    cb(null, require('./containers/Doc.js').default);
+  }, 'Doc');
+};
+
+// const Doc = Loadable({
+//   'loader': () => import('./containers/Doc.js'),
+//   'loading': Loading
+// });
+
+// const Profile = Loadable({
+//   'loader': () => import('./containers/Profile.js'),
+//   'loading': Loading
+// });
+
+const Profile = (location, cb) => {
+    require.ensure([], (require) => {
+        cb(null, require('./containers/Profile.js').default);
+    }, 'Profile');
+};
 
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path='/' component={App}>
-        <IndexRoute component={Login} />
-        <Route path='/homepage' component={HomePage} />
-        <Route path='/writeDoc' component={WriteDoc} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/comment' component={Comment} />
-        <Route path='/:id' component={Doc} />
+      <Route path='/' getComponent={App}>
+        <IndexRoute getComponent={Login} />
+        <Route path='/homepage' getComponent={HomePage} />
+        <Route path='/writeDoc' getComponent={WriteDoc} />
+        <Route path='/profile' getComponent={Profile} />
+        <Route path='/comment' getComponent={Comment} />
+        <Route path='/:id' getComponent={Doc} />
       </Route>
     </Router>
   </Provider>,
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,37 +1,38 @@
-var path = require('path');
-var webpack = require('webpack');
-var ROOT = path.resolve(__dirname);
-var htmlwebpackplugin = require('html-webpack-plugin');
-var cleanwebpackplugin = require('clean-webpack-plugin');
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
-var ENTRY = path.resolve(ROOT, 'src', 'index.js');
-var SRC = path.resolve(ROOT, 'src');
-var DIST = path.resolve(ROOT, 'dist');
-var DLL = '/dll/dll.vendor.js';
-var manifest = require('./dll/vendor-manifest.json');
+const ROOT = path.resolve(__dirname);
+const htmlwebpackplugin = require('html-webpack-plugin');
+const cleanwebpackplugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-var plugins = [
+const ENTRY = path.resolve(ROOT, 'src', 'index.js');
+const SRC = path.resolve(ROOT, 'src');
+const DIST = path.resolve(ROOT, 'dist');
+const DLL = '/dll/dll.vendor.js';
+const manifest = require('./dll/vendor-manifest.json');
+
+const plugins = [
         new htmlwebpackplugin(
             {
-                title: 'APP',
-                template: 'template.html',
-                chunks: ['index'],
-                filename: 'index.html',
-                vendor: DLL,
-                minify: {
-                    removeComments: true,
-                    collapseWhitespace: false,
+                'title': 'APP',
+                'template': 'template.html',
+                'chunks': ['index'],
+                'filename': 'index.html',
+                'vendor': DLL,
+                'minify': {
+                    'removeComments': true,
+                    'collapseWhitespace': false
                 }
             }
         ),
         new CopyWebpackPlugin([
-            {from: './dll', to:'./dll'}
+            { 'from': './dll', 'to': './dll' }
         ]),
         new MiniCssExtractPlugin({
-            filename: 'css/[name]_[hash:8].css',
-            allChunks: true
+            'filename': 'css/[name]_[hash:8].css',
+            'allChunks': true
         }),
         // webpack中-p代表--optimize-minimize也就是压缩的意思,cli中progress代表显示编译进度
         // webpack -p压缩的时候不会去掉一些注释，所以在这里可以设置一下，进一步压缩文件
@@ -58,35 +59,35 @@ var plugins = [
 
         // DefinePlugin()方法能创建可以在编译时配置的全局常量，这可能是非常有用的，允许开发版本和编译出的版本具有不同的行为
         // 在这里将环境设置为时'production'时，react会自动去掉没有用到的代码部分，让文件进一步精简
-        new webpack.DefinePlugin({
-         'process.env': {
-            NODE_ENV: JSON.stringify('production')
-          }
-        }),
+        // new webpack.DefinePlugin({
+        //  'process.env': {
+        //     'NODE_ENV': JSON.stringify('production')
+        //   }
+        // }),
         // new webpack.optimize.CommonsChunkPlugin({
         //     name: 'vendor',
         //     filename: 'vendor.bundle.js'
         // }),
         new webpack.DllReferencePlugin({
-            context: ROOT,
+            'context': ROOT,
             manifest
         }),
         new cleanwebpackplugin([DIST])
     ];
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        index: ENTRY
+    'mode': 'production',
+    'entry': {
+        'index': ENTRY
     },
-    output: {
-        path: DIST,
-        publicPath: '/',
-        filename: 'bundle_[name]_[hash:8].js' //结束最终JS文件
+    'output': {
+        'path': DIST,
+        'publicPath': '/',
+        'filename': 'bundle_[name]_[hash:8].js' // 结束最终JS文件
     },
 
-    resolve: {
-        alias: {
+    'resolve': {
+        'alias': {
             'action': path.resolve(__dirname, 'src/actions/index.js'),
             'containers': path.resolve(__dirname, 'src/containers/'),
             'components': path.resolve(__dirname, 'src/components/'),
@@ -95,54 +96,41 @@ module.exports = {
         }
     },
 
-    module: {
-        rules: [
+    'module': {
+        'rules': [
             {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: [
+                'test': /\.jsx?$/,
+                'exclude': /node_modules/,
+                'use': [
                     {
-                        loader: "babel-loader", // 使用babel-loader这个loader
-                        options: {
-                            presets: ['es2015', 'react']
+                        'loader': 'babel-loader', // 使用babel-loader这个loader
+                        'options': {
+                            'presets': ['es2015', 'react']
                         }
                     }
                 ]
             },
             {
-                test: /\.(less|scss|css)$/,
-                include: SRC,
-                use: [
+                'test': /\.(less|scss|css)$/,
+                'include': SRC,
+                'use': [
                     MiniCssExtractPlugin.loader,
                     'css-loader?minimize',
                     {
-                        loader:'postcss-loader',
-                        options: {           // 如果没有options这个选项将会报错 No PostCSS Config found
-                            plugins: (loader) => [
-                                require('autoprefixer')(), //CSS浏览器兼容
+                        'loader': 'postcss-loader',
+                        'options': { // 如果没有options这个选项将会报错 No PostCSS Config found
+                            'plugins': loader => [
+                                require('autoprefixer')() // CSS浏览器兼容
                             ]
                         }
                     }
                 ]
-                // use: ExtractTextPlugin.extract({
-                //     fallback: 'style-loader',
-                //     use: ['css-loader?minimize',
-                //         {
-                //             loader:'postcss-loader',
-                //             options: {           // 如果没有options这个选项将会报错 No PostCSS Config found
-                //                 plugins: (loader) => [
-                //                     require('autoprefixer')(), //CSS浏览器兼容
-                //                 ]
-                //             }
-                //         }
-                //     ]
-                // })
             },
             {
-                test: /\.(png|jpg|gif)$/,
-                use: ['url-loader?limit=10000&name=assets/[name]_[hash:8].[ext]']
+                'test': /\.(png|jpg|gif)$/,
+                'use': ['url-loader?limit=10000&name=assets/[name]_[hash:8].[ext]']
             }
         ]
     },
-    plugins: plugins
+    'plugins': plugins
 };
